e39c51edeb546fa7e22db5ff11e021bbfe1d46c4
andre shen <andregomars@gmail.com>
2021-01-18 23:22:33 -0800

    add layout page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const About = () => {
+    return (
+        <Layout>
+            <h1>here the about</h1>
+        </Layout>
+    )
+}
+
+export default About;
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Contact = () => {
+    return (
+        <Layout>
+            <h1>here the contact</h1>
+        </Layout>
+    )
+}
+
+export default Contact;
