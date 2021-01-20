60688eafb32c400fdd85960457e3fddf52dc0c48
andre shen <andregomars@gmail.com>
2021-01-19 23:34:57 -0800

    add styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import headerStyles from './header.module.scss';
 
 const Header = () => {
     return (
-        <div>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-            <li>
-                <Link to="/contact">Contact</Link>
-            </li>
-        </div>
+        <header className={headerStyles.header}>
+            <h1>Andre Header</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link className={headerStyles.link} to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
