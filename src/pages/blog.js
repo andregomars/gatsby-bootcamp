68bc75c2dfdd9df8d290601b53c8e6954f44233d
andre shen <andregomars@gmail.com>
2021-01-26 23:29:51 -0800

    show front matter from md in blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout';
 
 const Blog = () => {
+    const { allMarkdownRemark } = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+        `);
+
+    console.log(allMarkdownRemark);
     return (
-        <div>
-            <Layout>
-                <h1>here the blog</h1>
-            </Layout>
-        </div>
+        <Layout>
+            <h1>Blog</h1>
+            <ol>
+                {allMarkdownRemark.edges.map(({node}) => {
+                    const { frontmatter: { title, date }} = node;
+                    return (
+                        <li>
+                            <h2>{ title }</h2>
+                            <p>{ date }</p>
+                        </li>
+                    )
+                })}
+            </ol>
+        </Layout>
     )
 }
 
